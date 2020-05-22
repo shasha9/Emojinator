f2771767ce35014a646bee59d7f08e722d596612
Shasha Sami <shasha.sami91199@gmail.com>
2020-05-22 23:40:05 +0530

    Update Emojinator.py

--- Emojinator.py
+++ Emojinator.py
@@ -20,18 +20,18 @@ def main():
         median = cv2.GaussianBlur(gray, (5, 5), 0)
 
         kernel_square = np.ones((5, 5), np.uint8)
-        dilation = cv2.dilate(median, kernel_square, iterations=2)
-        opening = cv2.morphologyEx(dilation, cv2.MORPH_CLOSE, kernel_square)
-        ret, thresh = cv2.threshold(opening, 30, 255, cv2.THRESH_BINARY)
+        dilation =  cv2.dilate(median, kernel_square, iterations=2)
+        opening =  cv2.morphologyEx(dilation, cv2.MORPH_CLOSE, kernel_square)
+        ret, thresh =  cv2.threshold(opening, 30, 255, cv2.THRESH_BINARY)
 
-        thresh = thresh[y:y + h, x:x + w]
-        contours = cv2.findContours(thresh.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)[1]
+        thresh =  thresh[y:y + h, x:x + w]
+        contours =  cv2.findContours(thresh.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)[1]
         if len(contours) > 0:
-            contour = max(contours, key=cv2.contourArea)
+            contour =  max(contours, key=cv2.contourArea)
             if cv2.contourArea(contour) > 2500:
-                x, y, w1, h1 = cv2.boundingRect(contour)
+                x, y, w1, h1 =  cv2.boundingRect(contour)
                 newImage = thresh[y:y + h1, x:x + w1]
-                newImage = cv2.resize(newImage, (50, 50))
+                newImage =  cv2.resize(newImage, (50, 50))
                 pred_probab, pred_class = keras_predict(model, newImage)
                 print(pred_class, pred_probab)
                 img = overlay(img, emojis[pred_class], 400, 250, 90, 90)
